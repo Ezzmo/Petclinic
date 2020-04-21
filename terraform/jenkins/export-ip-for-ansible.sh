11b88d2eff6c584ff12e7ea162138c3a7c99d62b
Mo Ezzeddin <moezzeddin@gmail.com>
2020-04-21 14:09:48 +0100

    added depenancies role for ansible

--- terraform/jenkins/export-ip-for-ansible.sh
+++ terraform/jenkins/export-ip-for-ansible.sh
@@ -9,3 +9,5 @@ echo "[jenkins:vars]" >> ansible/inventory
 echo "ansible_user=mizan" >> ansible/inventory
 echo "ansible_ssh_common_args='-o StrictHostKeyChecking=no'" >> ansible/inventory
 echo "ansible_ssh_private_key_file=~/.ssh/id_rsa" >> ansible/inventory
+
+
