f85bbcf7b7d6ef2a8613a8eefbb58c0d2d03628e
Lungisani Reginald Phungula <reggie1997@hotmail.co.uk>
2020-04-23 17:56:16 +0100

    Update karma.conf.js

--- spring-petclinic-frontend/karma.conf.js
+++ spring-petclinic-frontend/karma.conf.js
@@ -6,31 +6,32 @@ module.exports = function (config) {
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
+      require('karma-coverage'),
       require('karma-jasmine'),
-      require('karma-phantomjs-launcher'),
       require('karma-chrome-launcher'),
+      require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client:{
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: true // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
       fixWebpackSourcePaths: true
     },
-    coverageReporter: {
-      type: "text",
-      file: "cov.txt"
+    coverageReporter:{
+      type:'text',
+      file: 'Coverage.txt'
     },
 
-    reporters: ['progress', 'kjhtml', 'coverage'],
+    reporters: ['progress', 'kjhtml','coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['PhantomJS'],
-    singleRun: true
+    browsers: ['PhasntomJS'],
+    singleRun: true 
   });
 };
